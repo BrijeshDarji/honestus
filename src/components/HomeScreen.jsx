d20e81e907585dd95e228dbff08e37f4c9704d10
Brijesh Darji <brijeshdarjiprofessional@gmail.com>
2024-02-27 23:02:33 +0530

    feat(home screen): :art: made page partial responsive

--- src/components/HomeScreen.jsx
+++ src/components/HomeScreen.jsx
@@ -74,44 +74,52 @@ function HomeScreen() {
                 </div>
             </div>
 
-            <div className='relative w-full'>
+            <div className="relative w-full">
                 <img
                     src={TestingAggregator}
                     alt="TestingAggregator"
-                    className='w-full h-180 object-cover bg-opacity-20'
+                    className="w-full h-180 object-cover bg-opacity-20"
                 />
 
-                <div className='absolute text-shadow shadow-[#00000040] inset-y-20 right-72 text-white font-semibold text-6.5xl flex flex-col items-start leading-relaxed'>
+                <div className="absolute text-shadow shadow-[#00000040] inset-y-20 right-72 text-white font-semibold text-6.5xl flex flex-col items-start leading-relaxed">
                     <div>End to End</div>
                     <div>Paperless</div>
                     <div>Process</div>
                 </div>
             </div>
 
-            <div className='flex justify-between my-6 mx-44'>
-                <div className='w-auto flex items-start flex-col'>
-                    <div className='flex items-center justify-center font-extrabold'>
-                        <span className='text-darkOrange text-8xl text-shadow-md shadow-[#00000047]'>10+</span>
-                        <span className='text-4.5xl ml-5'>Expert Partner Labs across India</span>
+            <div className="flex flex-col md:flex-row justify-between my-6 mx-6 md:mx-44">
+                <div className="w-auto flex items-start flex-col">
+                    <div className="flex items-center justify-center font-extrabold">
+                        <span className="text-darkOrange text-6xl md:text-8xl text-shadow-md shadow-[#00000047]">
+                            10+
+                        </span>
+                        <span className="text-4.5xl ml-5">
+                            Expert Partner Labs across India
+                        </span>
                     </div>
 
-                    <div className='font-medium text-3.5xl my-5'>
+                    <div className="font-medium text-2xl md:text-3.5xl my-5">
                         Trusted by numerous construction companies
                     </div>
 
-                    <div className='text-darkSlate text-2xl font-roboto'>
-                        Honestus made our project seamless by connecting us with a reliable material testing lab and taking care of all the logistics.
+                    <div className="text-darkSlate text-xl md:text-2xl font-roboto">
+                        Honestus made our project seamless by connecting us with
+                        a reliable material testing lab and taking care of all
+                        the logistics.
                     </div>
 
-                    <div className='font-medium text-darkOrange text-2xl mt-3'>
+                    <div className="font-medium text-darkOrange text-xl md:text-2xl mt-3">
                         Pratik Sinha
                     </div>
                 </div>
 
-                <div className='w-fit flex flex-col justify-end items-end'>
+                <div className="w-fit flex flex-col justify-end items-end">
                     <div>
-                        <div className="text-8.5xl font-medium ml-6 -mb-8">A+</div>
-                        <div className='flex'>
+                        <div className="text-8.5xl font-medium ml-6 -mb-8 md:text-8.5xl">
+                            A+
+                        </div>
+                        <div className="flex">
                             {
                                 Array.from({ length: 5 }, (v, i) => (
                                     <div
@@ -127,62 +135,58 @@ function HomeScreen() {
                 </div>
             </div>
 
-            <div className='bg-darkOrange flex flex-col items-center self-stretch gap-12 px-40 py-16'>
-                <div className="text-center text-white text-4.5xl font-medium">
+            <div className="bg-darkOrange flex flex-col items-center self-stretch gap-12 px-6 md:px-40 py-16">
+                <div className="text-center text-white text-3xl md:text-4.5xl font-medium">
                     Why do we exist?
                 </div>
 
-                <div className='flex justify-center items-start content-start gap-6 self-stretch flex-wrap'>
+                <div className="flex justify-center items-start content-start gap-6 self-stretch flex-wrap">
                     {
-                        WHY_WE_EXISTS.map((card, index) => {
-                            return (
-                                <div
-                                    key={card.title + index}
-                                    className="shadow-xl rounded-lg p-6 bg-white flex flex-col items-start self-stretch w-66"
-                                >
-                                    <img
-                                        src={card.icon}
-                                        alt={card.altText}
-                                        className="w-12 h-12"
-                                    />
+                        WHY_WE_EXISTS.map((card, index) => (
+                            <div
+                                key={card.title + index}
+                                className="shadow-xl rounded-lg p-6 bg-white flex flex-col items-start self-stretch w-full md:w-66"
+                            >
+                                <img
+                                    src={card.icon}
+                                    alt={card.altText}
+                                    className="w-12 h-12"
+                                />
 
-                                    <div className="text-1.5xl font-medium">
-                                        {card.title}
-                                    </div>
-
-                                    <div className="text-base text-darkSlate">
-                                        {card.description}
-                                    </div>
+                                <div className="text-xl font-medium">
+                                    {card.title}
                                 </div>
-                            )
-                        })
+
+                                <div className="text-base text-darkSlate">
+                                    {card.description}
+                                </div>
+                            </div>
+                        ))
                     }
                 </div>
             </div>
 
-            <div className='flex flex-col items-center self-stretch gap-12 px-40 py-16'>
-                <div className="text-center text-4.5xl font-medium">
+            <div className="flex flex-col items-center self-stretch gap-12 px-6 md:px-40 py-16">
+                <div className="text-center text-3xl md:text-4.5xl font-medium">
                     What do we offer?
                 </div>
 
                 <div className="flex justify-center">
-                    <div className='flex items-start justify-center flex-wrap self-stretch gap-12'>
-                        <div className="flex flex-col justify-center items-start self-stretch gap-7 w-105">
+                    <div className="flex items-start justify-center flex-wrap self-stretch gap-12">
+                        <div className="flex flex-col justify-center items-start self-stretch gap-7 w-full md:w-105">
                             {
-                                WE_OFFER.map((content, index) => {
-                                    return (
-                                        <div
-                                            key={index}
-                                            className="bg-darkOrange flex justify-center items-center gap-2 p-5 text-white text-xl rounded-lg w-full"
-                                        >
-                                            {content}
-                                        </div>
-                                    )
-                                })
+                                WE_OFFER.map((content, index) => (
+                                    <div
+                                        key={index}
+                                        className="bg-darkOrange flex justify-center items-center gap-2 p-5 text-white text-lg md:text-xl rounded-lg w-full"
+                                    >
+                                        {content}
+                                    </div>
+                                ))
                             }
                         </div>
 
-                        <div className="w-105">
+                        <div className="w-full md:w-105">
                             <img
                                 src={WhatWeOffer}
                                 alt="WhatWeOffer"
@@ -192,56 +196,57 @@ function HomeScreen() {
                 </div>
             </div>
 
-            <div className='bg-darkOrange flex flex-col justify-center items-center gap-6 self-stretch px-40 py-25'>
-                <div className="text-center text-white text-4.5xl font-medium">
+            <div className="bg-darkOrange flex flex-col justify-center items-center gap-6 self-stretch px-6 md:px-40 py-25">
+                <div className="text-center text-white text-3xl md:text-4.5xl font-medium">
                     Benefits
                 </div>
 
-                <div className='flex justify-center items-start content-start flex-wrap gap-12 self-stretch '>
+                <div className="flex justify-center items-start content-start flex-wrap gap-12 self-stretch ">
                     {
-                        BENEFITS.map((card, index) => {
-                            return (
-                                <div
-                                    key={card.altText + index}
-                                    className='flex flex-col justify-center items-center gap-6 shadow-xl bg-white rounded-lg p-6 h-60 w-75'
-                                >
-                                    <div className="flex justify-center">
-                                        <img
-                                            src={card.icon}
-                                            alt={card.altText}
-                                            className="w-16 h-16"
-                                        />
-                                    </div>
-
-                                    <div className="text-xl">
-                                        {card.description}
-                                    </div>
+                        BENEFITS.map((card, index) => (
+                            <div
+                                key={card.altText + index}
+                                className="flex flex-col justify-center items-center gap-6 shadow-xl bg-white rounded-lg p-6 h-60 w-full md:w-75"
+                            >
+                                <div className="flex justify-center">
+                                    <img
+                                        src={card.icon}
+                                        alt={card.altText}
+                                        className="w-16 h-16"
+                                    />
                                 </div>
-                            )
-                        })
+
+                                <div className="text-lg md:text-xl">
+                                    {card.description}
+                                </div>
+                            </div>
+                        ))
                     }
                 </div>
             </div>
 
-            <div className='flex items-center justify-center self-stretch gap-37 px-40 py-25'>
-                <div className='flex items-center justify-center flex-wrap self-stretch gap-25'>
-                    <div className="flex flex-col justify-center items-start self-stretch gap-12 w-105">
-                        <div className="text-3.5xl font-semibold">
+            <div className="flex items-center justify-center self-stretch gap-6 px-6 md:px-40 py-25">
+                <div className="flex items-center justify-center flex-wrap self-stretch gap-6 md:gap-25">
+                    <div className="flex flex-col justify-center items-start self-stretch gap-6 w-full md:w-105">
+                        <div className="text-3xl md:text-3.5xl font-semibold">
                             Partner with Us
                         </div>
 
-                        <div className="text-xl">
-                            Our commitment to high standards of quality and reliability in partnering with material testing labs simplifies the construction process and earns the trust of our clients.
+                        <div className="text-lg md:text-xl">
+                            Our commitment to high standards of quality and
+                            reliability in partnering with material testing labs
+                            simplifies the construction process and earns the
+                            trust of our clients.
                         </div>
 
                         <button className="bg-darkOrange rounded-lg flex justify-center items-center gap-2 px-8 py-3">
-                            <div className="text-white text-xl font-semibold">
+                            <div className="text-white text-xl md:text-2xl font-semibold">
                                 Join our Partner Lab Network
                             </div>
                         </button>
                     </div>
 
-                    <div className="w-105">
+                    <div className="w-full md:w-105">
                         <img
                             src={PartnerWithUs}
                             alt="PartnerWithUs"
