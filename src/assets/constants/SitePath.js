c0e89f18ebd4d381176935b1b63667df722076f7
mpmeetpatel <meetpatel.professional@gmail.com>
2024-03-18 13:50:37 +0530

    delete files

--- src/assets/constants/SitePath.js
+++
@@ -1,8 +0,0 @@
-/**
- *  @description This file contains route URLs.
- */
-
-export const URL_HOME_SCREEN = '/'
-export const URL_LOGIN = '/login'
-export const URL_SIGN_UP = '/signup'
-export const URL_CUSTOMER_PLACE_ORDER = '/customer-place-order'
